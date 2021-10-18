d37ddcae60ec3f73abb862e925b605caf78efedd
alifakbarr <alifakbar8800@gmail.com>
2021-10-18 10:04:02 +0700

    make supplier/status/detailTolak

---
+++ resources/views/supplier/status/detailTolak.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts/supplier/main-layout')
+@section('title', 'Pengadaan Kursi Kantor untuk Laboratorium Komputer')
+@section('content')
+<table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">Kode</th>
+      <td class="fw-bold">PBA-0001</td>
+    </tr>
+    <tr>
+      <th scope="row">Status</th>
+      <td><div class="badge badge-danger text-decoration-none">Tolak</div></td>
+    </tr>
+    <tr>
+      <th scope="row">Nama Paket</th>
+      <td>Pengadaan Komputer dan Serve untuk Laboratorium Komputer</td>
+    </tr>
+    <tr>
+      <th scope="row">Barang</th>
+      <td>Komputer</td>
+    </tr>
+    <tr>
+      <th scope="row">Jumlah</th>
+      <td>10</td>
+    </tr>
+    <tr>
+      <th scope="row">Harga Penawaran</th>
+      <td>Rp 50.000.000,-</td>
+    </tr>
+    <tr>
+      <th scope="row">Harga Terkoreksi</th>
+      <td>Rp 45.000.000,-</td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td>
+        <button class="btn btn-sm btn-primary">Simpan</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+@endsection
